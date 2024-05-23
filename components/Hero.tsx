9fbe865dc3ad3ff4b630d828ae906540f2cb8f04
Yukihira2k <glen.mirandilla02@gmail.com>
2024-05-23 17:58:45 +0800

    fixed some bugs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,16 +27,13 @@ const Hero = () => {
 
         <div className="flex justify-center relative my-20 z-10 ">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center" >
-                <h2 className="uppercase tracking-widest text-xs text-center text-blue-11 max-w-80">
-                    Dynamic Web Magic with Next.js
-                </h2>
-
+              
                 <TextGenerateEffect 
                     className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                    words="Transforming Concepts into Seamless User Experiences"
+                    words="Transforming Visions into Interactive Realities"
                 />
 
-                <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi I&apos;m Glen, a next.js Developer based in philippines</p>
+                <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi I&apos;m Glen, a Next.js Developer based in philippines</p>
 
                 <a href="#about">
             <MagicButton
